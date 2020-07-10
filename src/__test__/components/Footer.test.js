cc8a340eb422890e066b28b27abff537e0898475
Juan Pablo Calao <jcalaop@gmail.com>
2020-07-10 10:15:47 -0500

    jest with react

---
+++ src/__test__/components/Footer.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
+import Footer from '../../components/Footer';
+
+describe('Footer Snapshot', () => {
+  test('Comprobar la UI del componente Footer', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
+  });
+});
+
+describe('<Footer />', () => {
+  const footer = mount(<Footer />);
+  test('Render del componente Footer', () => {
+    expect(footer.length).toEqual(1);
+  });
+  test('Render del título', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
---
+++ src/__test__/components/Header.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { create } from 'react-test-renderer';
+import ProviderMock from '../../__mocks__/ProviderMock';
+import Header from '../../components/Header';
+
+describe('<Header />', () => {
+  test('Comprobar el Snapshot de Header', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+
+    expect(header.toJSON()).toMatchSnapshot();
+  });
+  test('Render del componente Header', () => {
+    const header = shallow(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+    expect(header.length).toEqual(1);
+  });
+  test('Render del título', () => {
+    const header = mount(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>,
+    );
+    expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
